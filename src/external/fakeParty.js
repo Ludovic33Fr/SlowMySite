65087400d75e507ca0dfe4f39a470d8636fe4745
Ludovic LEFEBVRE <gothacrafty@msn.com>
2021-11-26 10:16:41 +0100

    Change impl of fakeParty 3

--- src/external/fakeParty.js
+++ src/external/fakeParty.js
@@ -1,12 +1,14 @@
 (function() {
 
 	// just place a div at top right
-	var div = document.createElement('div');
+	/*
+    var div = document.createElement('div');
 	div.style.position = 'fixed';
 	div.style.top = 0;
 	div.style.right = 0;
 	div.textContent = 'Injected!';
 	document.body.appendChild(div);
+    */
 
     var shouldRun = true;
     var desiredLoadFactor = .5;
