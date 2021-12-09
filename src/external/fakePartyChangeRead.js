5d4a9b0c267636d89b4ac0196044d12f43472c29
Ludovic LEFEBVRE <gothacrafty@msn.com>
2021-12-09 22:53:32 +0100

    new version of script

---
+++ src/external/fakePartyChangeRead.js
@@ -0,0 +1,62 @@
+(function() {
+
+	// just place a div at top right
+	/*
+    var div = document.createElement('div');
+	div.style.position = 'fixed';
+	div.style.top = 0;
+	div.style.right = 0;
+	div.textContent = 'Injected!';
+	document.body.appendChild(div);
+    */
+
+    var shouldRun = true;
+    //Number of second to wait
+    var desiredLoadFactor = 5;
+
+    var now = new Date().getTime();
+    var result = 0
+    console.log("Start the service : " + new Date().toLocaleString('en-US'));
+    while(shouldRun) {
+        result += Math.random() * Math.random();
+        if (new Date().getTime() > now + 1000*desiredLoadFactor)
+        {
+            shouldRun = false;
+            console.log("End the service : " + new Date().toLocaleString('en-US'));
+    
+            if (document !== null && document.body !== null)
+            {
+                var all = document.getElementsByTagName("*");
+                for (var i=0, max=all.length; i < max; i++) {
+                    // Do something with the element here
+                }
+                var keypad1 = document.createElement("div");
+                keypad1.innerHTML = "...Bad it is too slow ...";
+                document.body.appendChild(keypad1);
+            } else if( document.readyState !== 'loading' ) {
+                var all = document.getElementsByTagName("*");
+                for (var i=0, max=all.length; i < max; i++) {
+                    // Do something with the element here
+                }
+                document.addEventListener("DOMContentLoaded", function(event) { 
+                    var keypad1 = document.createElement("div");
+                    keypad1.innerHTML = "...Bad it is too slow ...";
+                    document.body.appendChild(keypad1);
+                  });
+            } else {
+                var all = document.getElementsByTagName("*");
+                for (var i=0, max=all.length; i < max; i++) {
+                    // Do something with the element here
+                }
+                document.addEventListener("DOMContentLoaded", function(event) { 
+                var keypad1 = document.createElement("div");
+                keypad1.innerHTML = "...Bad it is too slow ...";
+                document.body.appendChild(keypad1);
+              });
+            }
+        
+            
+        }
+    }	
+
+})();
